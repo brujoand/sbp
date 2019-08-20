52703242e2b18cb35b26f503b0ffad72c7d0344d
brujoand <anders@brujordet.no>
2019-08-20 11:10:22 +0200

    fix broken tests due to different env

--- tests/segments/path.bash
+++ tests/segments/path.bash
@@ -7,8 +7,9 @@ export -f pretty_print_segment
 function test_that_we_get_full_path() {
   local result
   local wdir=${PWD/$HOME/\~}
-  local paths=$(echo ${wdir//\//  } | perl -pe 's/^\s\s/ /')
+  local paths=${wdir//\//  }
+  local clean_paths=${paths/^  / }
   export settings_path_max_length=50
   result=$("${sbp_path}/segments/path.bash" 0 0)
-  assert_equals " $paths " "$result"
+  assert_equals " $clean_paths " "$result"
 }
