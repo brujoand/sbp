ec1fccdcf159946b14a0951d18a034e46361e221
brujoand <anders@brujordet.no>
2019-08-20 11:16:33 +0200

    Try some black magic

--- tests/segments/path.bash
+++ tests/segments/path.bash
@@ -8,7 +8,7 @@ function test_that_we_get_full_path() {
   local result
   local wdir=${PWD/$HOME/\~}
   local paths=${wdir//\//  }
-  local clean_paths=${paths/^  / }
+  local clean_paths=$(echo "$paths" | sed 's/^  / /')
   export settings_path_max_length=50
   result=$("${sbp_path}/segments/path.bash" 0 0)
   assert_equals " $clean_paths " "$result"
