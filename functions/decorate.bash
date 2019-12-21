e7faefce1b21d18087fb948da211ed66412195c2
brujoand <anders@brujordet.no>
2019-12-21 15:26:35 +0100

    Adapt the theme files to the proper base16 scheme

--- functions/decorate.bash
+++ functions/decorate.bash
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
 
-export colors_ids=( 00 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 )
+export colors_ids=( 00 01 02 03 04 05 06 07 08 09 0A 0B 0C 0D 0E 0F )
 
 get_complement_rgb() {
   input_colors=()
