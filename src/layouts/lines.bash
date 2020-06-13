dcb15b5a451f5e8acfb6a0fb7bd7e95b35b6aac5
brujoand <anders@brujordet.no>
2020-06-13 14:30:35 +0200

    Fix the lines layout

--- src/layouts/lines.bash
+++ src/layouts/lines.bash
@@ -5,36 +5,55 @@ SETTINGS_SEGMENT_SPLITTER_RIGHT='-'
 SETTINGS_PROMPT_PREFIX_UPPER='┍'
 SETTINGS_PROMPT_PREFIX_LOWER='└'
 SETTINGS_PROMPT_READY_ICON="${SETTINGS_PROMPT_PREFIX_LOWER}${SETTINGS_PROMPT_READY_ICON}"
-SETTINGS_SEGMENT_ENABLE_BG_COLOR=0
-SETTINGS_PATH_SPLITTER_DISABLE=1
-SETTINGS_PROMPT_READY_NEWLINE=1
 SETTINGS_GIT_ICON=' '
 
+print_themed_filler() {
+  local -n return_value=$1
+  local filler_size=$2
+  # Account for seperator and padding
+  padding=$(printf "%*s" "$filler_size")
+  SEGMENT_LINE_POSITION=2
+  prompt_filler_output="$(print_themed_segment 'normal' "$padding")"
+  mapfile -t segment_output <<< "$prompt_filler_output"
+
+  return_value=${segment_output[1]}
+}
 
 print_themed_segment() {
-  local primary_color=$1
-  local secondary_color=$2
-  local segment_value=$3
-  local position=$4
-  local segment_length=$5
+  local color_type=$1
+  shift
+  local segment_value="${*}"
+  local segment_length=${#segment_value}
+  local prefix_size=0
 
-  if [[ -n "${segment_value// /}" ]]; then
-    segment_length=$(( segment_length + 2 ))
-    segment_value=" ${segment_value} "
+  if [[ "$color_type" == 'highlight' ]]; then
+    PRIMARY_COLOR="$PRIMARY_COLOR_HIGHLIGHT"
+  fi
+
+  if [[ -z "${segment_value// /}" || "$segment_value" == "$SETTINGS_PROMPT_READY_ICON" ]]; then
+    SETTINGS_SEGMENT_SEPARATOR_RIGHT=''
+    SETTINGS_SEGMENT_SEPARATOR_LEFT=''
+  fi
+
+  seperator_size=$(( ${#SETTINGS_SEGMENT_SEPARATOR_RIGHT} + ${#SETTINGS_SEGMENT_SEPARATOR_LEFT} ))
+
+  if [[ "$SEGMENT_LINE_POSITION" == 1 ]]; then
+    if [[ "$segment_value" == "$SETTINGS_PROMPT_READY_ICON" ]]; then
+      segment_value="${segment_value} "
+      segment_length=$(( segment_length + 1 ))
+    else
+      prefix="$SETTINGS_PROMPT_PREFIX_UPPER"
+    fi
+    prefix_size=${#prefix}
   fi
 
 
-  secondary_color="$primary_color"
-  primary_color=""
-  color="$(print_fg_color "$secondary_color")"
+  local color
+  decorate::print_fg_color 'color' "$PRIMARY_COLOR"
 
-  if [[ -z "${segment_value// /}" || "$position" == 'line2' ]]; then
-    full_output="${color}${segment_value}"
-  else
-    full_output="${color}${SETTINGS_SEGMENT_SEPARATOR_RIGHT}${segment_value}${SETTINGS_SEGMENT_SEPARATOR_LEFT}"
-    segment_length=$(( segment_length + 2))
-  fi
+  full_output="${prefix}${color}${SETTINGS_SEGMENT_SEPARATOR_RIGHT}${segment_value}${SETTINGS_SEGMENT_SEPARATOR_LEFT}"
+  segment_length=$(( segment_length + seperator_size + prefix_size ))
 
-  printf '%s;;%s' "$segment_length" "$full_output"
+  printf '%s\n%s' "$segment_length" "$full_output"
 }
 
