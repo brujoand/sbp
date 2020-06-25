f29dc298f38ecd7effba4220dad5b19b1cce0ee4
brujoand <anders@brujordet.no>
2020-06-25 10:49:18 +0200

    Update autocompletion

--- src/interact.bash
+++ src/interact.bash
@@ -5,18 +5,22 @@ _sbp_print_usage() {
   Usage: sbp [command]
 
   Commands:
-  reload          - Reload SBP and user settings
-  edit-config     - Opens the sbp config in \$EDITOR (${EDITOR:-'not set'})
-  edit-colors     - Opens the colors config in \$EDITOR (${EDITOR:-'not set'})
-  show-status     - Show the current configuration
-  show-help       - Show this help text
-  list-segments   - List all available segments
-  list-hooks      - List all available hooks
-  list-themes     - List all available color themes and layouts
-  set-color       - Set [color] for the current session
-  set-layout      - Set [layout] for the current session
-  toggle-peekaboo - Toggle visibility of [segment] or [hook]
-  toggle-debug    - Toggle debug mode
+  reload            - Reload SBP and user settings
+  status            - Show the current configuration
+  help              - Show this help text
+  list
+    segments        - List all available segments
+    hooks           - List all available hooks
+    themes          - List all available color themes and layouts
+  edit
+    config          - Opens the sbp config in \$EDITOR (${EDITOR:-'not set'})
+    colors          - Opens the colors config in \$EDITOR (${EDITOR:-'not set'})
+  set
+    color           - Set [color] for the current session
+    layout          - Set [layout] for the current session
+  toggle
+    peekaboo        - Toggle visibility of [segment] or [hook]
+    debug           - Toggle debug mode
 EOF
 }
 
@@ -81,45 +85,73 @@ _sbp_peekaboo() {
 
 sbp() {
   case $1 in
-    'list-segments') # Show all available segments
-      "$_sbp_themed_helper" 'list_segments'
+    'list')
+      case $2 in
+        'segments') # Show all available segments
+          "$_sbp_themed_helper" 'list_segments'
+          ;;
+        'hooks') # Show all available hooks
+          "$_sbp_themed_helper" 'list_hooks'
+          ;;
+        'themes') # Show all defined colors and layouts
+          "$_sbp_themed_helper" 'list_themes'
+          ;;
+        *)
+          _sbp_print_usage && return 1
+          ;;
+      esac
       ;;
-    'list-hooks') # Show all available hooks
-      "$_sbp_themed_helper" 'list_hooks'
+    'toggle')
+      case $2 in
+        'peekabo')
+          _sbp_require_argument "$2" '[segment|hook]'
+          _sbp_peekaboo "$2"
+          ;;
+        'debug') # Toggle debug mode
+          _sbp_toggle_debug
+          ;;
+        *)
+          _sbp_print_usage && return 1
+          ;;
+      esac
       ;;
-    'toggle-peekaboo')
-      _sbp_require_argument "$2" '[segment|hook]'
-      _sbp_peekaboo "$2"
+    'set')
+      case $2 in
+        'color') # Show currently defined colors
+          _sbp_require_argument "$2" '[color]'
+          export SBP_THEME_COLOR_OVERRIDE="$2"
+          _sbp_reload
+          ;;
+        'layout')
+          _sbp_require_argument "$2" '[layout]'
+          export SBP_THEME_LAYOUT_OVERRIDE="$2"
+          _sbp_reload
+          ;;
+        *)
+          _sbp_print_usage && return 1
+          ;;
+      esac
       ;;
-    'set-color') # Show currently defined colors
-      _sbp_require_argument "$2" '[color]'
-      export SBP_THEME_COLOR_OVERRIDE="$2"
-      _sbp_reload
-      ;;
-    'set-layout')
-      _sbp_require_argument "$2" '[layout]'
-      export SBP_THEME_LAYOUT_OVERRIDE="$2"
-      _sbp_reload
-      ;;
-    'list-themes') # Show all defined colors and layouts
-      "$_sbp_themed_helper" 'list_themes'
+    'edit')
+      case $2 in
+        'config') # Open the config file
+          _sbp_edit_config
+          ;;
+        'colors') # Open the config file
+          _sbp_edit_colors
+          ;;
+        *)
+          _sbp_print_usage && return 1
+          ;;
+      esac
       ;;
     'reload') # Reload settings and SBP
       _sbp_reload
       ;;
-    'edit-config') # Open the config file
-      _sbp_edit_config
-      ;;
-    'edit-colors') # Open the config file
-      _sbp_edit_colors
-      ;;
-    'toggle-debug') # Toggle debug mode
-      _sbp_toggle_debug
-      ;;
     'extra_options') # Woho, hiddden function
       "$_sbp_themed_helper" 'generate_extra_options'
       ;;
-    'show-status')
+    'status')
       "$_sbp_themed_helper" 'show_status'
       ;;
     *)
@@ -136,7 +168,19 @@ _sbp() {
 
   words=()
   case "$prev" in
-    'toggle-peekaboo')
+    'set')
+      words=('layout' 'color')
+      ;;
+    'list')
+      words=('hooks' 'segments' 'themes')
+      ;;
+    'toggle')
+      words=('debug' 'peekabo')
+      ;;
+    'edit')
+      words=('config' 'colors')
+      ;;
+    'peekaboo')
       for hook in $($_sbp_themed_helper list_words 'hooks'); do
         words+=("$hook")
       done
@@ -145,18 +189,18 @@ _sbp() {
         words+=("$segment")
       done
       ;;
-    'set-color')
+    'color')
       for color in $($_sbp_themed_helper list_words 'colors'); do
         words+=("$color")
       done
       ;;
-    'set-layout')
+    'layout')
       for layout in $($_sbp_themed_helper list_words 'layouts'); do
         words+=("$layout")
       done
       ;;
     *)
-      words=('list-segments' 'list-hooks' 'toggle-peekaboo' 'set-color' 'set-layout' 'list-themes' 'reload' 'help' 'edit-config' 'edit-colors' 'show-status' 'toggle-debug')
+      words=('set' 'list' 'toggle' 'edit' 'reload' 'help' 'status')
       ;;
   esac
 
