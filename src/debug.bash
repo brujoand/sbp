8ad29eefbeb446ebd8da1d3d9ed6994f16914afb
brujoand <anders@brujordet.no>
2020-07-09 08:28:50 +0200

    Try to build on osx

--- src/debug.bash
+++ src/debug.bash
@@ -1,11 +1,5 @@
 #! /usr/bin/env bash
 
-if [[ "$OSTYPE" == "darwin"* ]]; then
-  date_cmd='gdate'
-else
-  date_cmd='date'
-fi
-
 debug::log_error() {
   local timestamp=$(date +'%y.%m.%d %H:%M:%S')
   local context="${timestamp}:${BASH_SOURCE[1]}:${FUNCNAME[1]}"
@@ -18,11 +12,23 @@ debug::log_info() {
   >&2 printf '%s: \e[38;5;76m%s\e[00m\n' "${context}" "${*}"
 }
 
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  if type -P gdate &>/dev/null; then
+    date_cmd='gdate'
+  else
+    debug::log_error "OSX date does not support ms granularity, please install gdate"
+    date_cmd=''
+  fi
+else
+  date_cmd='date'
+fi
+
 debug::start_timer() {
   timer_start=$("$date_cmd" +'%s%3N')
 }
 
 debug::tick_timer() {
+  [[ -z "$date_cmd" ]] && return 0
   timer_stop=$("$date_cmd" +'%s%3N')
   timer_spent=$(( timer_stop - timer_start))
   >&2 echo "${timer_spent}ms: $1"
