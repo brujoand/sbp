a7d489a1ef4ea9ee38552a24c436c4f798e3bdcc
Sandro Jäckel <sandro.jaeckel@gmail.com>
2020-08-02 17:30:12 +0200

    Add BSD, MacOS compatiblity

--- src/segments/load.bash
+++ src/segments/load.bash
@@ -3,12 +3,37 @@
 # Adapted from liquidprompts load average
 # https://github.com/nojhan/liquidprompt/blob/deff598f30f097279d6f6959ba49441923dec041/liquidprompt
 
-CPU_COUNT=$(nproc 2>/dev/null || \grep -c '^[Pp]rocessor' /proc/cpuinfo)
+segments::init_load() {
+  case "$(uname -s)" in
+    Darwin | FreeBSD | OpenBSD)
+      CPU_COUNT=$(sysctl -n hw.ncpu)
+
+      get_load_average() {
+        local bol eol IFS=$' \t'
+        # shellcheck disable=SC2034,SC2162
+        read bol LOAD_AVERAGE eol <<<"$(LC_ALL=C sysctl -n vm.loadavg)"
+      }
+      ;;
+    Linux)
+      CPU_COUNT=$(nproc 2>/dev/null || \grep -c '^[Pp]rocessor' /proc/cpuinfo)
+
+      get_load_average() {
+        local eol IFS=$' \t'
+        # shellcheck disable=SC2034,SC2162
+        read LOAD_AVERAGE eol </proc/loadavg
+      }
+      ;;
+    *)
+      # TODO inform the user that his system is not supported
+      ;;
+  esac
+}
 
 segments::load() {
-  local eol IFS=$' \t'
-  # shellcheck disable=SC2034,2162
-  read LOAD_AVERAGE eol </proc/loadavg
+  segments::init_load
+
+  local LOAD_AVERAGE
+  get_load_average
   LOAD_AVERAGE=${LOAD_AVERAGE/./}
   LOAD_AVERAGE=${LOAD_AVERAGE#0}
   LOAD_AVERAGE=${LOAD_AVERAGE#0}
