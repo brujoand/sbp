cb84689dfef82372e4fc5746ec69f3758eb900a4
Flying Lollipop <fllp@posteo.org>
2024-01-22 17:06:20 +0100

    fix: use array for config, document segment

--- src/segments/terraform.bash
+++ src/segments/terraform.bash
@@ -1,9 +1,24 @@
 #! /usr/bin/env bash
 
-tf_icon="${SEGMENTS_TF_ICON:-}"
-tf_extenstion="${TF_FILE_EXTENCION:-tf}"
-scanned_dirs="${SEGMENTS_TF_SCANNED_DIRS:-$PWD}"
+if [[ ${SEGMENTS_USE_NERDFONTS} == 1 ]]; then
+  # use Terraform icon from Nerdfonts
+  tf_icon=""
+else
+  tf_icon="${SEGMENTS_TF_ICON:-⊤}"
+fi
+
+tf_extension="${TF_FILE_EXTENSION:-tf}"
 declare tf_version=""
+declare -a scanned_dirs=()
+
+# To configure more than one dir to be scanned
+# an array is necessary so we get values expanded
+# to real arguments when calling find later
+if [[ -n ${SEGMENTS_TF_SCANNED_DIRS} ]]; then
+  scanned_dirs=("${SEGMENTS_TF_SCANNED_DIRS[@]}")
+else
+  scanned_dirs=("$PWD")
+fi
 
 _tf_available() {
   command -v terraform
@@ -28,7 +43,7 @@ _get_terraform_version() {
 }
 
 segments::terraform() {
-  if [[ $(_tf_available) ]] && [[ -n $(find ${scanned_dirs} -maxdepth 1 -name "*.${tf_extenstion}" -print -quit 2>/dev/null) ]]; then
+  if [[ $(_tf_available) ]] && [[ -n $(find "${scanned_dirs[@]}" -maxdepth 1 -name "*.${tf_extension}" -print -quit 2>/dev/null) ]]; then
     _get_terraform_version
     segment="$tf_icon $tf_version"
     print_themed_segment 'normal' "$segment"
